03926216d20368547a0b3b70465cc6efb4c60b58
Kaden Brown <bro22001@byui.edu>
2022-09-24 16:17:03 -0600

    Add files via upload

---
+++ scripts/principles.js
@@ -0,0 +1,5 @@
+let last =new Date(document.lastModified);
+document.getElementById("last").innerHTML= last;
+let year = new Date();
+year= year.getFullYear();
+document.querySelector('#year').textContent = year;
