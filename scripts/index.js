0b1aa78766258af092c3b17f082ee1bb77ce6a30
kaden530 <bro22001@byui.edu>
2022-09-30 16:35:17 -0600

    added CSS

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,14 @@ document.getElementById("last").innerHTML= last;
 let year = new Date();
 year= year.getFullYear();
 document.querySelector('#year').textContent = year;
+
+let click = document.querySelector('.navbtn');
+let list = document.getElementById('nav-drop');
+click.addEventListener("click",()=>{
+    list.classList.toggle('newlist');});
+
+function toggle() {
+    let list = document.getElementById("nav-drop")
+    list.classList.toggle(".newlist");
+      }
+      
