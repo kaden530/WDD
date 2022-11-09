21b096663c3f509d4028b9e605dbde58e1a9404e
kaden530 <bro22001@byui.edu>
2022-11-09 16:00:53 -0700

    Update prophets.js

--- Week09/prophets.js
+++ Week09/prophets.js
@@ -24,7 +24,7 @@ const prophets = getprophets();
     info.textContent= ` ${prophet.birthdate} - ${prophet.death} `
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname + ' - ' + prophet.order + 'Latter-day President.');
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname + ' - ' + prophet.order + ' Latter-day President.');
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
